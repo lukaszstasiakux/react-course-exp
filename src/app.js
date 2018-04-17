99ab3dab40b913ac936ba1eca47a1ef2469166f0
“Lukasz <lukasz.stasiak@librato.com>
2018-04-17 08:56:59 +0200

    remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -11,17 +11,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water bill', amount: 4500}))
-store.dispatch(addExpense({description: 'Gas bill',amount: 4000, createdAt: 1000}))
-store.dispatch(addExpense({description: 'Rent bill', amount: 109500}))
-
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-console.log(visibleExpenses)
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
